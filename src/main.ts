126bf4867dee5c47755dafe791873b5ad119ab8b
Aaron <apbruno@ucsc.edu>
2023-10-18 15:47:55 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Sticker Sketchpad";
-
-document.title = "Aaron's Game: " + gameName;
+const gameName = "Aaron Bruno's Sticker Sketchpad";
+document.title = gameName;
+const canvasHeight = 256;
+const canvasWidth = 256;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
+
+const canvasContext = canvas.getContext("2d");
+canvasContext!.fillStyle = "white";
+canvasContext?.fillRect(0, 0, 256, 256);
+
+app.append(canvas);
